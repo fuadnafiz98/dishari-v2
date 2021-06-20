90cf440060ccfa06bc53321bc9865640d0548cb9
nishat1098 <nishanul116@gmail.com>
2021-06-20 13:45:03 +0600

    play_pause_added

--- app/src/main/java/org/tensorflow/lite/examples/detection/Help.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/Help.java
@@ -6,11 +6,13 @@ import androidx.cardview.widget.CardView;
 
 import android.content.ClipData;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -20,30 +22,49 @@ import org.tensorflow.lite.examples.detection.Features;
 public class Help extends AppCompatActivity implements View.OnClickListener{
     private CardView notifyObsCard;
     public boolean playing = false;
+
+    //MediaPlayer newMediaPlayer;
+    ImageView playIcon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
-        setContentView(R.layout.activity_help);
+
+/*new approach with toggle */
+        final MediaPlayer newMediaPlayer = MediaPlayer.create(this, R.raw.demo_google);
+playIcon = findViewById(R.id.obj_dict_media_play);
+playIcon.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        if(!newMediaPlayer.isPlaying()){
+            newMediaPlayer.start();
+            playIcon.setImageResource(R.drawable.pause);
+        }else{
+            newMediaPlayer.pause();
+            playIcon.setImageResource(R.drawable.play_button);
+        }
+    }
+});
 
 
 
         /*first tutorial*/
-
-        final MediaPlayer objDetectMP = MediaPlayer.create(this, R.raw.demo_google);
-        FloatingActionButton playObjDetect = (FloatingActionButton) this.findViewById(R.id.floatingObjDetect);
-        playObjDetect.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                if(playing == false) {
-                    objDetectMP.start();
-                    playing = true;
-                } else {
-                    objDetectMP.stop();
-                    playing = false;
-                }
-            }
-        });
+//
+//        final MediaPlayer objDetectMP = MediaPlayer.create(this, R.raw.demo_google);
+//        FloatingActionButton playObjDetect = (FloatingActionButton) this.findViewById(R.id.floatingObjDetect);
+//        playObjDetect.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                if(playing == false) {
+//                    objDetectMP.start();
+//                    playing = true;
+//                } else {
+//                    objDetectMP.stop();
+//                    playing = false;
+//                }
+//            }
+//        });
 
         /*second tutorial*/
         final MediaPlayer distMeasureMP = MediaPlayer.create(this,R.raw.demo_google);
