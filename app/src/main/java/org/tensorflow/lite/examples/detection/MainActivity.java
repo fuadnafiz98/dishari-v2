c59fb2dbcb99b691c5a2490a52448ee9c99edba1
mohianprotik <mohianprotik@gmail.com>
2021-06-20 19:56:14 +0600

    ok

--- app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
