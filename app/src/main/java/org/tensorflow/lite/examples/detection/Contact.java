d3fb5e1ab3da9207b744002a270f6467e9956685
mohianprotik <mohianprotik@gmail.com>
2021-06-20 21:51:49 +0600

    contact edit updated

--- app/src/main/java/org/tensorflow/lite/examples/detection/Contact.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/Contact.java
@@ -3,12 +3,47 @@ package org.tensorflow.lite.examples.detection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Contact extends AppCompatActivity {
+    private static final String File_Name = "contact.txt";
+    EditText mobile1;
+    EditText mobile2;
+    EditText mobile3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        mobile1 = findViewById(R.id.mobile1);
+        mobile2 = findViewById(R.id.mobile2);
+        mobile3 = findViewById(R.id.mobile3);
+    }
+    public void save(View v){
+        String text = mobile1.getText().toString() +"\n" + mobile2.getText().toString() +"\n"+ mobile3.getText().toString();
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(File_Name,MODE_PRIVATE);
+            fos.write(text.getBytes());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(fos!= null){
+                try{
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
